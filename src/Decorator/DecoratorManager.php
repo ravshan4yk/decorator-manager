2b9921976143dba9116c461c7b308e408df331fa
Ravshan Khaydarov <khaydarov92@gmail.com>
2018-06-16 02:32:38 +0500

    init repo

---
+++ src/Decorator/DecoratorManager.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace src\Decorator;
+
+use DateTime;
+use Exception;
+use Psr\Cache\CacheItemPoolInterface;
+use Psr\Log\LoggerInterface;
+use src\Integration\DataProvider;
+
+class DecoratorManager extends DataProvider
+{
+    private $cache;
+    private $logger;
+
+    /**
+     * @param CacheItemPoolInterface $cache
+     */
+    public function setCache(CacheItemPoolInterface $cache)
+    {
+        $this->cache = $cache;
+    }
+
+    /**
+     * @param LoggerInterface $logger
+     */
+    public function setLogger(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getResponse(array $input)
+    {
+        try {
+            $cacheKey = $this->getCacheKey($input);
+            $cacheItem = $this->cache->getItem($cacheKey);
+
+            if ($cacheItem->isHit()) {
+                return $cacheItem->get();
+            }
+
+            $result = $this->get($input);
+
+            $cacheItem
+                ->set($result)
+                ->expiresAt(
+                    (new DateTime())->modify('+1 day')
+                );
+
+            return $result;
+        } catch (Exception $e) {
+            $this->logger->critical('Error');
+        }
+
+        return [];
+    }
+
+    /**
+     * @param array $input
+     * @return string
+     */
+    public function getCacheKey(array $input)
+    {
+        return json_encode($input);
+    }
+}
